6a2056868e0b7872122abb27d336e5bdf7a98429
Abid052 <136680895+Abid052@users.noreply.github.com>
2023-06-24 15:38:46 +0600

    Add files via upload

---
+++ splash.h
@@ -0,0 +1,15 @@
+#include<dos.h>
+
+void printDesign(void)
+ {
+    printf("                          ********************************************\n");
+    printf("                         *        Restaurant Management System       *\n");
+    printf("                          ********************************************\n");
+    printf("                         *                 Welcome!                  *\n");
+    printf("                         *              Loading...                   *\n");
+    printf("                         ********************************************\n\n");
+    sleep(5);
+    system("cls");
+
+ }
+
